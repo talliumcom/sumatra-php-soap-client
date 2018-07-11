69502fc8c39599e46f6a819baa95b39c5251b2c4
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2018-07-11 17:50:04 +0300

    Removes '_argument' postfix in client methods

--- src/Sumatra/SumatraClient.php
+++ src/Sumatra/SumatraClient.php
@@ -13,17 +13,17 @@ class SumatraClient extends Client
 
     public function kunden_abgleich(\Sumatra\Type\abgleich_argument $abgleich_argument): \Sumatra\Type\kunden_abgleich_response
     {
-        return $this->call('abgleich_argument', $abgleich_argument);
+        return $this->call('abgleich', $abgleich_argument);
     }
 
     public function kunden_aenderungen(\Sumatra\Type\aenderungen_argument $aenderungen_argument): \Sumatra\Type\kunden_aenderungen_response
     {
-        return $this->call('aenderungen_argument', $aenderungen_argument);
+        return $this->call('aenderungen', $aenderungen_argument);
     }
 
     public function kunden_abfrage(\Sumatra\Type\abfrage_argument $abfrage_argument): \Sumatra\Type\kunden_abfrage_response
     {
-        return $this->call('abfrage_argument', $abfrage_argument);
+        return $this->call('abfrage', $abfrage_argument);
     }
 
     public function vermittler_einwilligung(\Sumatra\Type\vermittler_einwilligung_argument $vermittler_einwilligung_argument): \Sumatra\Type\vermittler_einwilligung_response
@@ -33,21 +33,21 @@ class SumatraClient extends Client
 
     public function vermittler_abgleich(\Sumatra\Type\abgleich_argument $abgleich_argument): \Sumatra\Type\vermittler_abgleich_response
     {
-        return $this->call('abgleich_argument', $abgleich_argument);
+        return $this->call('abgleich', $abgleich_argument);
     }
 
     public function vermittler_aenderungen(\Sumatra\Type\aenderungen_argument $aenderungen_argument): \Sumatra\Type\vermittler_aenderungen_response
     {
-        return $this->call('aenderungen_argument', $aenderungen_argument);
+        return $this->call('aenderungen', $aenderungen_argument);
     }
 
     public function vermittler_abfrage(\Sumatra\Type\abfrage_argument $abfrage_argument): \Sumatra\Type\vermittler_abfrage_response
     {
-        return $this->call('abfrage_argument', $abfrage_argument);
+        return $this->call('abfrage', $abfrage_argument);
     }
 
     public function kundenvertraege_uebermittlung(\Sumatra\Type\kundenvertraege_uebermittlung_argument $kundenvertraege_uebermittlung_argument): \Sumatra\Type\kundenvertraege_uebermittlung_response
     {
-        return $this->call('kundenvertraege_uebermittlung_argument', $kundenvertraege_uebermittlung_argument);
+        return $this->call('kundenvertraege_uebermittlung', $kundenvertraege_uebermittlung_argument);
     }
 }
